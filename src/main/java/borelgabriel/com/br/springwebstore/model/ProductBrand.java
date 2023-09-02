a776af4456dd9b9f4314054cad0585d2106f911b
Gabriel Borel <biel_borel@hotmail.com>
2023-09-02 19:17:32 -0300

    feat: add ProductBrand model

---
+++ src/main/java/borelgabriel/com/br/springwebstore/model/ProductBrand.java
@@ -0,0 +1,35 @@
+package borelgabriel.com.br.springwebstore.model;
+
+import jakarta.persistence.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "product_brand")
+@SequenceGenerator(name = "product_brand_seq", sequenceName = "product_brand_seq", allocationSize = 1)
+public class ProductBrand implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "product_brand_seq")
+    private Long id;
+    private String nameDescription;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNameDescription() {
+        return nameDescription;
+    }
+
+    public void setNameDescription(String nameDescription) {
+        this.nameDescription = nameDescription;
+    }
+}
