4889913b821623f2da13f8abfe4c7278deacee70
Gabriel Borel <biel_borel@hotmail.com>
2023-09-02 17:33:45 -0300

    project started

---
+++ src/main/java/borelgabriel/com/br/springwebstore/SpringWebstoreApplication.java
@@ -0,0 +1,11 @@
+package borelgabriel.com.br.springwebstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebstoreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebstoreApplication.class, args);
+    }
+}
