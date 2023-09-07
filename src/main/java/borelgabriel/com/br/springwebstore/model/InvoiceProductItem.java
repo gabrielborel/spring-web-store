488aaae0b814d6b7020de22326df7b4d06e39787
Gabriel Borel <biel_borel@hotmail.com>
2023-09-07 14:04:29 -0300

    feat: add InvoiceProductItem mode class

---
+++ src/main/java/borelgabriel/com/br/springwebstore/model/InvoiceProductItem.java
@@ -0,0 +1,82 @@
+package borelgabriel.com.br.springwebstore.model;
+
+import jakarta.persistence.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "invoice_product_item")
+@SequenceGenerator(name = "invoice_product_item_seq", sequenceName = "invoice_product_item_seq", allocationSize = 1)
+public class InvoiceProductItem implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "invoice_product_item_seq")
+    private Long id;
+
+    @Column(nullable = false)
+    private Double quantity;
+
+    @ManyToOne
+    @JoinColumn(
+            name = "product_id",
+            nullable = false,
+            foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "product_fk")
+    )
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(
+            name = "purchase_invoice_id",
+            nullable = false,
+            foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "purchase_invoice_fk")
+    )
+    private PurchaseInvoice purchaseInvoice;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Double getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Double quantity) {
+        this.quantity = quantity;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public PurchaseInvoice getPurchaseInvoice() {
+        return purchaseInvoice;
+    }
+
+    public void setPurchaseInvoice(PurchaseInvoice purchaseInvoice) {
+        this.purchaseInvoice = purchaseInvoice;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof InvoiceProductItem that)) return false;
+        return Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
